ae5f3472fd3b4c2628824e670a3a79b20dd7add0
Michael Tremmel <mjt29@astro.washington.edu>
2015-09-21 16:25:10 -0700

    added orbit and mergers (for real now)

---
+++ Simpy/BlackHoles/mergers.py
@@ -0,0 +1,53 @@
+import numpy as np
+import readcol
+import os
+from .. import cosmology
+from .. import Files
+import pynbody
+
+def findmergers(outname,diagname = 'romulus8.3.out'):
+	os.system("awk '/BHSink/ && /Merge/ && /eating/' *"+diagname+"* > "+outname)
+	return
+
+def reducedata(simname, RetData = False,outname = 'out',mergename='BHmerge.txt'):
+	if not os.path.exists(mergename):
+		print "didn't find merger file... getting mergers from outputs"
+		findmergers(mergename,outname=outname)
+	if not os.path.exists('files.list'):
+		Files.getFileLists(simname)
+	f = open('files.list','r')
+	Files = f.readlines()
+	f.close()
+	f = open('steps.list','r')
+	steps = f.readlines()
+	f.close()
+
+	s = pynbody.load( Files[-1].strip('\n'))
+	lstep = float(steps[-1].strip('\n'))
+
+	simtime = cosmology.getTime(s.properties['a']**-1 -1,s)
+	dt = simtime/lstep
+	tunit = s.infer_original_units('Gyr')
+
+	a,b, ID, c, IDeat, d, time, e, f, kick, g, h, mratio = readcol.readcol(mergename,twod=False)
+	output = {}
+	time = pynbody.array.SimArray(time,tunit)
+	output['time']  =  time.in_units('Gyr')
+	
+	output['ID']    =  ID
+	output['IDeat'] =  IDeat
+	output['step']  =  time.in_units('Gyr')/dt
+	output['ratio'] =  mratio
+	output['kick']  =  kick
+	
+	outkeys = ['time', 'step', 'ID', 'IDeat', 'ratio', 'kick']
+	tofile = []
+	for key in outkeys:
+		tofile.append(output[key])
+	tofile = tuple(tofile)
+	print "saving to file..."
+        np.savetxt(simname+'.mergers',np.column_stack(tofile),fmt= ['%f','%f','%d','%d','%f','%f'])
+	if RetData==True: return output
+	else: return
+
+
