5f7a4ce47f3af4d11a9747509f549f4223c16d96
mtremmel <m.tremmel6@gmail.com>
2015-09-21 16:39:41 -0700

    reformatted BlackHoles files in pycharm

--- Simpy/BlackHoles/__init__.py
+++ Simpy/BlackHoles/__init__.py
@@ -1,53 +1,55 @@
 import pynbody
 import numpy as np
-import readcol
 import os
+import readcol
 from . import orbit, mergers
 from .. import *
 
 
-
-def writeBHMark(simname,step,Name=None,iord=False,massrange=False):
-        if not Name: f = open('BH.'+step+'.mark','w')
-        else: f = open(Name,'w')
-        s = pynbody.load(simname+'.'+step)
-        f.write(str(len(s))+' '+str(len(s.gas))+' '+str(len(s.star))+'\n')
-        if not iord:
-                if not massrange:
-                        bhind, = np.where(s.stars['tform']<0)
-                else:
-                        if len(massrange) != 2:
-                                print "error massrange must be a length 2 tuple!"
-                                return
-                        bhind, = np.where((s.stars['tform']<0)&(s.stars['mass'].in_units('Msol')<massrange[1])&(s.stars['mass'].in_units('Msol')>massrange[0]))
+def writebhmark(simname, step, Name=None, iord=False, massrange=False):
+    if not Name:
+        f = open('BH.' + step + '.mark', 'w')
+    else:
+        f = open(Name, 'w')
+    s = pynbody.load(simname + '.' + step)
+    f.write(str(len(s)) + ' ' + str(len(s.gas)) + ' ' + str(len(s.star)) + '\n')
+    if not iord:
+        if not massrange:
+            bhind, = np.where(s.stars['tform'] < 0)
         else:
-                bhind = np.array([])
-                for ii in range(len(iord)):
-                        tmpind, = np.where(s.stars['iord']==iord[ii])
-                        if len(tmpind)==0: print "uh oh... iord ", iord[ii]," not found!"
-                        bhind = np.append(bhind,tmpind)
-        bhindreal = bhind+len(s.dark)+len(s.gas)+1
-        for ii in range(len(bhindreal)):
-                f.write(str(bhindreal[ii])+'\n')
-        f.close()
-        del(s)
-        return
+            if len(massrange) != 2:
+                print "error massrange must be a length 2 tuple!"
+                return
+            bhind, = np.where((s.stars['tform'] < 0) & (s.stars['mass'].in_units('Msol') < massrange[1]) & (
+            s.stars['mass'].in_units('Msol') > massrange[0]))
+    else:
+        bhind = np.array([])
+        for ii in range(len(iord)):
+            tmpind, = np.where(s.stars['iord'] == iord[ii])
+            if len(tmpind) == 0: print "uh oh... iord ", iord[ii], " not found!"
+            bhind = np.append(bhind, tmpind)
+    bhindreal = bhind + len(s.dark) + len(s.gas) + 1
+    for ii in range(len(bhindreal)):
+        f.write(str(bhindreal[ii]) + '\n')
+    f.close()
+    del (s)
+    return
+
 
 def getBHiords(simname):
-        if not os.path.exists("BHid.list"):
-                print "finding IDs for all BHs that ever existed..."
-                os.system("awk '{print $1}' "+simname+".orbit > BHid.list")
-                f = open("BHid.list",'r')
-                id = f.readlines()
-                id = np.array(id)
-                id = id.astype('int')
-                id = np.unique(id)
-                f.close()
-                os.system("rm BHid.list")
-                np.savetxt("BHid.list",id)
-        else:
-                print "previous BHid.list file found! reading it..."
-                id, = readcol.readcol("BHid.list",twod=False)
+    if not os.path.exists("BHid.list"):
+        print "finding IDs for all BHs that ever existed..."
+        os.system("awk '{print $1}' " + simname + ".orbit > BHid.list")
+        f = open("BHid.list", 'r')
+        id = f.readlines()
+        id = np.array(id)
+        id = id.astype('int')
+        id = np.unique(id)
+        f.close()
+        os.system("rm BHid.list")
+        np.savetxt("BHid.list", id)
+    else:
+        print "previous BHid.list file found! reading it..."
+        id, = readcol.readcol("BHid.list", twod=False)
 
-        return id 
-
+    return id
--- Simpy/BlackHoles/orbit.py
+++ Simpy/BlackHoles/orbit.py
@@ -1,138 +1,145 @@
 import numpy as np
-#import matplotlib.pyplot as plt
 import pynbody
-#from pynbody.analysis import profile, angmom, halo
-#from pynbody import snapshot, filt, units, config, array
-#from scipy import optimize as opt
-import warnings
-import math
+
 import os
-#from pynbody.analysis import pkdgrav_cosmo as cosmo
+# from pynbody.analysis import pkdgrav_cosmo as cosmo
 import pickle
 import readcol
 import gc
 from .. import Files
 from .. import util
 
-#orbitcolumns = {'iord':1,'time':2,'step':3,'mass':4,'pos':[5,6,7],'vel':[8,9,10],'mdot':12,'dm':13,'dt':15,'a':16}
 
-def sepOrbitbyStep(file,min=0,max=1000000000):
-	if not os.path.exists('orbitsteps'): os.system('mkdir orbitsteps')
-	os.chdir('orbitsteps')
-	print "separating orbit file by step..."
-	os.system("awk -F ' ' '{if($3!=int($3) && $3 >= "+str(min)+" && $3 <= "+str(max)+") print>(int($3)+1); if($3==int($3) && $3 >= "+str(min)+" && $3 <= "+str(max)+") print>$3}' "+'../'+file)
-	os.chdir('../')
-	return
+def sepOrbitbyStep(file, min=0, max=1000000000):
+    if not os.path.exists('orbitsteps'): os.system('mkdir orbitsteps')
+    os.chdir('orbitsteps')
+    print "separating orbit file by step..."
+    os.system("awk -F ' ' '{if($3!=int($3) && $3 >= " + str(min) + " && $3 <= " + str(
+        max) + ") print>(int($3)+1); if($3==int($3) && $3 >= " + str(min) + " && $3 <= " + str(
+        max) + ") print>$3}' " + '../' + file)
+    os.chdir('../')
+    return
 
-def getOrbitValbyStep(simname,minstep=1,maxstep=4096,clean=True,filename=None,ret_output=False):
-        '''
+
+def getOrbitValbyStep(simname, minstep=1, maxstep=4096, clean=True, filename=None, ret_output=False):
+    '''
 	simname = name of simulation (i.e. simname.004096)
 	minstep, maxstep = min, max steps the code will expect to have in the orbit file. 
 	clean = True/False, clean up the extra files made from the oritinal orbit file (this is done to spare memory, but they take up lots of space...
         '''
-	ea = []
-	output = {'iord':[],'time':[],'step':[],'mass':[],'x':[],'y':[],'z':[],'vx':[],'vy':[],'vz':[],'mdot':[],'mdotmean':[],'mdotsig':[],'a':[]}
-	for i in range(minstep,maxstep+1):
-		if os.path.exists('orbitsteps/'+str(i)):
-			print "getting data for step ", i
-			bhid,time,step,mass,x,y,z,vx,vy,vz,pot,mdot,dm,E,dt,a = readcol.readcol('orbitsteps/'+str(i),twod=False)
-			if clean==True: os.system('rm orbitsteps/'+str(i))
-		else: continue
-		ord = np.argsort(bhid)
-		bhid = bhid[ord].astype(np.int64)
-		time = time[ord]
-		step = step[ord]
-		mass = mass[ord]
-		x = x[ord]
-		y = y[ord]
-		z = z[ord]
-		vx = vx[ord]
-		vy = vy[ord]
-		vz = vz[ord]
-		mdot = mdot[ord]
-		dt = dt[ord]
-		a = a[ord]
-		ubhid,uind= np.unique(bhid,return_index=True)	
-		for ii in range(len(ubhid)):
-			if ii < len(ubhid)-1: idind = range(uind[ii],uind[ii+1])
-			else: idind = range(uind[ii],len(step))
-			curstep, = np.where(step[idind]==i)
-			if len(curstep)==0:continue
-			curstep = curstep[0]
-			output['iord'].append(ubhid[ii])
-	                output['step'].append(step[idind][curstep])
-	                output['mass'].append(mass[idind][curstep])
-	                output['time'].append(time[idind][curstep])
-	                output['x'].append(x[idind][curstep])
-	                output['y'].append(y[idind][curstep])
-	                output['z'].append(z[idind][curstep])
-	                output['vx'].append(vx[idind][curstep])
-	                output['vy'].append(vy[idind][curstep])
-	                output['vz'].append(vz[idind][curstep])
-	                output['mdot'].append(mdot[idind][curstep])
-	                output['a'].append(a[idind][curstep])
-			utimes, ind = np.unique(time[idind],return_index=True)
-			mean,std = util.timeweightedAve(mdot[idind][ind],dt[idind][ind])
-			output['mdotmean'].append(mean)
-                        output['mdotsig'].append(std)
-	if filename:
-		f = open(filename,'wb')
-		pickle.dump(output, f)
-		f.close()
-	if ret_output:
-		return output
+    ea = []
+    output = {'iord': [], 'time': [], 'step': [], 'mass': [], 'x': [], 'y': [], 'z': [], 'vx': [], 'vy': [], 'vz': [],
+              'mdot': [], 'mdotmean': [], 'mdotsig': [], 'a': []}
+    for i in range(minstep, maxstep + 1):
+        if os.path.exists('orbitsteps/' + str(i)):
+            print "getting data for step ", i
+            bhid, time, step, mass, x, y, z, vx, vy, vz, pot, mdot, dm, E, dt, a = readcol.readcol(
+                'orbitsteps/' + str(i), twod=False)
+            if clean == True: os.system('rm orbitsteps/' + str(i))
+        else:
+            continue
+        ord = np.argsort(bhid)
+        bhid = bhid[ord].astype(np.int64)
+        time = time[ord]
+        step = step[ord]
+        mass = mass[ord]
+        x = x[ord]
+        y = y[ord]
+        z = z[ord]
+        vx = vx[ord]
+        vy = vy[ord]
+        vz = vz[ord]
+        mdot = mdot[ord]
+        dt = dt[ord]
+        a = a[ord]
+        ubhid, uind = np.unique(bhid, return_index=True)
+        for ii in range(len(ubhid)):
+            if ii < len(ubhid) - 1:
+                idind = range(uind[ii], uind[ii + 1])
+            else:
+                idind = range(uind[ii], len(step))
+            curstep, = np.where(step[idind] == i)
+            if len(curstep) == 0: continue
+            curstep = curstep[0]
+            output['iord'].append(ubhid[ii])
+            output['step'].append(step[idind][curstep])
+            output['mass'].append(mass[idind][curstep])
+            output['time'].append(time[idind][curstep])
+            output['x'].append(x[idind][curstep])
+            output['y'].append(y[idind][curstep])
+            output['z'].append(z[idind][curstep])
+            output['vx'].append(vx[idind][curstep])
+            output['vy'].append(vy[idind][curstep])
+            output['vz'].append(vz[idind][curstep])
+            output['mdot'].append(mdot[idind][curstep])
+            output['a'].append(a[idind][curstep])
+            utimes, ind = np.unique(time[idind], return_index=True)
+            mean, std = util.timeweightedAve(mdot[idind][ind], dt[idind][ind])
+            output['mdotmean'].append(mean)
+            output['mdotsig'].append(std)
+    if filename:
+        f = open(filename, 'wb')
+        pickle.dump(output, f)
+        f.close()
+    if ret_output:
+        return output
 
-def truncOrbitFile(simname,ret_output=False):
-	output = getOrbitValbyStep(simname)
-	tofile = []
-	for key in output:
-		tofile.append(output[key])
-	if ret_output==False:
-		del(output)
-		gc.collect()
-	tofile = tuple(tofile)
-	outputname = simname+'.shortened.orbit'
-	print "saving to file..."
-	np.savetxt(outputname,np.column_stack(tofile))
-	del(tofile)
-	gc.collect()
-	if ret_output==True:
-		return output
-	return
 
-def sticthOrbitSteps(simname,nfiles, ret_output=False,overwrite=True, nstart=1):
-	ea = np.array([])
-	output = {'iord':ea,'time':ea,'step':ea,'mass':ea,'x':ea,'y':ea,'z':ea,'vx':ea,'vy':ea,'vz':ea,'mdot':ea,'mdotmean':ea,'mdotsig':ea,'a':ea}
-	outorder = ['iord','time','step','mass','x','y','z','vx','vy','vz','mdot','mdotmean','mdotsig','a']
-	outputname = simname+'.shortened.orbit'
+def truncOrbitFile(simname, ret_output=False):
+    output = getOrbitValbyStep(simname)
+    tofile = []
+    for key in output:
+        tofile.append(output[key])
+    if ret_output == False:
+        del (output)
+        gc.collect()
+    tofile = tuple(tofile)
+    outputname = simname + '.shortened.orbit'
+    print "saving to file..."
+    np.savetxt(outputname, np.column_stack(tofile))
+    del (tofile)
+    gc.collect()
+    if ret_output == True:
+        return output
+    return
 
-	if os.path.exists(outputname) and overwrite==False:
-		cols = {'iord':1,'time':2,'step':3,'mass':4,'x':5,'y':6,'z':7,'vx':8,'vy':9,'vz':10,'mdot':11,'mdotmean':12,'mdotsig':13,'a':14}
-		data = readcol.readcol(simname+'.shortened.orbit')
-		for key in output.keys():
-			output[key] = data[:,cols[key]-1]
 
-	for i in range(nfiles):
-		num = i+nstart
-		f = open(simname+'.shortened.orbit'+str(num),'rb')
-		output_part = pickle.load(f)
-		f.close()
-		for key in output.keys():
-			output[key] = np.append(output[key],output_part[key])
-		del(output_part)
-		gc.collect()
-	tofile = []
-        for key in outorder:
-                tofile.append(output[key])
-	tofile = tuple(tofile)
+def sticthOrbitSteps(simname, nfiles, ret_output=False, overwrite=True, nstart=1):
+    ea = np.array([])
+    output = {'iord': ea, 'time': ea, 'step': ea, 'mass': ea, 'x': ea, 'y': ea, 'z': ea, 'vx': ea, 'vy': ea, 'vz': ea,
+              'mdot': ea, 'mdotmean': ea, 'mdotsig': ea, 'a': ea}
+    outorder = ['iord', 'time', 'step', 'mass', 'x', 'y', 'z', 'vx', 'vy', 'vz', 'mdot', 'mdotmean', 'mdotsig', 'a']
+    outputname = simname + '.shortened.orbit'
 
-	print "saving to file..."
-        np.savetxt(outputname,np.column_stack(tofile),fmt=['%d','%f','%d','%e','%f','%f','%f','%f','%f','%f','%e','%e','%e','%f'])
-	del(tofile)
-	gc.collect()
-	if ret_output==True:
-                return output
-	else:
-		del(output)
-                gc.collect()
-		return
+    if os.path.exists(outputname) and overwrite == False:
+        cols = {'iord': 1, 'time': 2, 'step': 3, 'mass': 4, 'x': 5, 'y': 6, 'z': 7, 'vx': 8, 'vy': 9, 'vz': 10,
+                'mdot': 11, 'mdotmean': 12, 'mdotsig': 13, 'a': 14}
+        data = readcol.readcol(simname + '.shortened.orbit')
+        for key in output.keys():
+            output[key] = data[:, cols[key] - 1]
+
+    for i in range(nfiles):
+        num = i + nstart
+        f = open(simname + '.shortened.orbit' + str(num), 'rb')
+        output_part = pickle.load(f)
+        f.close()
+        for key in output.keys():
+            output[key] = np.append(output[key], output_part[key])
+        del (output_part)
+        gc.collect()
+    tofile = []
+    for key in outorder:
+        tofile.append(output[key])
+    tofile = tuple(tofile)
+
+    print "saving to file..."
+    np.savetxt(outputname, np.column_stack(tofile),
+               fmt=['%d', '%f', '%d', '%e', '%f', '%f', '%f', '%f', '%f', '%f', '%e', '%e', '%e', '%f'])
+    del (tofile)
+    gc.collect()
+    if ret_output == True:
+        return output
+    else:
+        del (output)
+        gc.collect()
+        return
