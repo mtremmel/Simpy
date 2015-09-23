df448e0d3225a2cab1dd07fb8d42e95253dc98ee
mtremmel <m.tremmel6@gmail.com>
2015-09-23 09:29:09 -0700

    added new functions to easily create symbolic links, run make_xml for NCHILADA files. Fixed other functions to deal with NCHILADA files

--- Simpy/Files.py
+++ Simpy/Files.py
@@ -1,14 +1,24 @@
 import os
 
 
-def getFileLists(simname):
-        simname_split = simname.split('.')
-        num = len(simname_split)
-        os.system('ls  *.iord | cut -d"." -f1-'+str(num+1)+' > files.list')
-        os.system('ls *.iord | cut -d"." -f'+str(num+1)+' > steps.list')
-	os.system('ls '+simname+'.00*.grp | cut -d "." -f1-'+str(num+1)+ '> grpfiles.list' )
+def getFileLists(parentdir, simname, NCHILADA=True):
+	totalpath = parentdir + simname
+	simname_split = totalpath.split('.')
+	num = len(simname_split)
+	if not NCHILADA:
+		os.system('ls ' + parentdir + '*.iord | cut -d"." -f1-' + str(num + 1) + ' > files.list')
+		os.system('ls ' + parentdir + '*.iord | cut -d"." -f' + str(num + 1) + ' > steps.list')
+	else:
+		os.system('ls ' + parentdir + simname+'.00* --directory > files.list')
+		os.system('ls ' + parentdir + simname + '.00* --directory | cut -d"." -f' + str(num + 1) +' > steps.list')
+	os.system('ls ' + parentdir + simname +'.00*.grp | cut -d "." -f1-' + str(num + 1) + '> grpfiles.list' )
 	return
 
+
+def cklists(parentdir,simname,NCHILADA=True):
+	if not os.path.exists('files.list'):
+		print "files,steps lists not found, running getFileLists..."
+		getFileLists(parentdir, simname, NCHILADA=NCHILADA)
 
 
 def mkAbridge(filename,columns=[1,2,3,4],condition=None,suffix=None):
@@ -31,3 +41,30 @@ def mkAbridge(filename,columns=[1,2,3,4],condition=None,suffix=None):
 		print cstr
 		os.system(cstr)
 	return
+
+
+def makelinks(parentdir, simname):
+	cklists(parentdir, simname, NCHILADA=True)
+	f = open('files.list', 'r')
+	files = f.readlines()
+	f.close()
+	f = open('steps.list', 'r')
+	steps = f.readlines()
+	f.close()
+	for i in range(len(files)):
+		os.system('ln -s ' + files[i].strip('\n') + ' ' + simname + '.' + steps[i].strip('\n'))
+
+
+def mkXML(do='All',start=0):
+	wdir = os.getcwd()
+	cklists(parentdir, simname, NCHILADA=True)
+	f = open('files.list','r')
+	files = f.readlines()
+	if do == 'All': num = len(files)
+	else: num = do
+	for i in range(num)+start:
+		os.chdir(files[i].strip('\n'))
+		os.system('~trq/bin/make_xml.pl')
+	os.chdir(wdir)
+	return
+
