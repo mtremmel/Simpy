cf211bd5b81ec2715a9bce851cc0a846983314ff
mtremmel <m.tremmel6@gmail.com>
2015-10-01 08:02:48 -0700

    fix issues with redshift SFH plot

--- Simpy/Stars/plots.py
+++ Simpy/Stars/plots.py
@@ -9,7 +9,7 @@ def plt_SFH(sim, color='b', linestyle='solid', lw=3,bins=50, trange=None, label=
 	if type =='time':
 		if trange is None:
 			trange = [0,tform.max()]
-		dt = (trange[1] - trange[0])*1e9/bins
+		dt = (trange[1] - trange[0])*1e9/float(bins)
 		data = plotting.plt.hist(tform, range=trange, bins=bins, linestyle=linestyle, color=color,label=label,histtype='step',weights=massform/dt, linewidth=lw)
 		sfr = data[0]
 		tedges = data[1]
@@ -17,20 +17,15 @@ def plt_SFH(sim, color='b', linestyle='solid', lw=3,bins=50, trange=None, label=
 
 
 	if type == 'redshift':
-		aform, zform = cosmology.getScaleFactor(tform,sim)
 		if trange is None:
-			trange = [0,zform.max()]
-		n, zbins = np.histogram(zform, range=trange, bins=bins)
-		tedges = np.array([cosmology.getTime(z, sim) for z in zbins])
+			trange = [0,25]
+		dz = (trange[1]-trange[0])/float(bins)
+		zbins = np.arange(trange[0],trange[1]+dt,dt)
+		tedges = np.array([cosmology.getTime(z,sim) for z in zbins])
 		dt = (tedges[1:] - tedges[0:-1]) * 1e9
-		sfr = n*massform/dt
-		if xlog is False:
-			plotting.plt.step(zbins[0:-1], sfr, linestyle=linestyle, color=color, label=label, linewidth=lw, where='post')
-			plotting.plt.xlabel('z',fontsize=30)
-		else:
-			plotting.plt.step(zbins[0:-1]+1, sfr, linestyle=linestyle, color=color, label=label, linewidth=lw, where='post')
-			plotting.plt.xlabel('z + 1',fontsize=30)
-
+		data = np.histogram(tform, range=trange, bins=tedges,weights=massform)
+		sfr = data[0]/dt
+		plotting.plt.step(zbins[0:-1],sfr,linestyle=linestyle, color=color, label=label, linewidth=lw, where='post')
 
 	plotting.plt.ylabel('SFR [M$_{\odot}$ yr$^{-1}$]')
 	if xlog:
