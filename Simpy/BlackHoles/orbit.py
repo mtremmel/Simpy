fixed fix tform sign check in all_BH_tform

Simpy/BlackHoles/orbit.py
@@ -261,9 +261,8 @@ class Orbit(object):
 		sl = pynbody.tipsy.StarLog(self.simname+'.starlog')
 		ord = np.argsort(sl['iord'])
 		bhind, = np.where(np.in1d(sl['iord'][ord],self.bhiords))
-		if len(np.where(sl['tform'][ord][bhind]>0))>0:
-			print "WARNING! Positive tforms found! Something wrong with starlog file matching"
 		self.tform = sl['tform'][ord][bhind] * -1
+		if self.tform.min() < 0: print "WARNING! Positive tforms were found for BHs!"
 		del sl
 		gc.collect()
 