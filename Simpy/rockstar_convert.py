56dce30f639977b7850185e213cd67d2612edf74
mtremmel <m.tremmel6@gmail.com>
2015-10-28 10:30:51 -0700

    finish rockstar convert package to convert the modified rockstar output (halo_particles files) to halo_particles_fpos files for quicker halo extraction with less memory in pynbody/database runs

--- Simpy/rockstar_convert.py
+++ Simpy/rockstar_convert.py
@@ -5,7 +5,7 @@ import os
 
 def rockstar_iord_to_fpos(snap):
 	iord_to_fpos = util.init_iord_to_fpos(snap)
-	f = open(snap+'rockstar.halo_partiels',rb)
+	f = open(snap+'rockstar.halo_particles','rb')
 	orig = np.fromfile(f,dtype=np.dtype('int64'),count=-1)
 	f.close()
 	return iord_to_fpos[orig]
@@ -15,9 +15,15 @@ def convert_all_rockstar():
 	files = f.readlines()
 	f.close()
 	for ff in files:
-		print ff
 		snap = ff.strip('\n')
-		if not os.path.exists(snap+'rockstar.halo_partiels'):
+		print snap
+		if not os.path.exists(snap+'.rockstar.halo_particles'):
 			print "snapshot does not have halo_particles file... ignoring this snapshot..."
 			continue
 		fpos = rockstar_iord_to_fpos(snap)
+		if type(fpos[-1]) != np.int64:
+			fpos = fpos.astype(np.int64)
+		outf = open(snap+'.rockstar.halo_particles_fpos','wb')
+		fpos.tofile(outf, format='%l')
+		outf.close()
+
