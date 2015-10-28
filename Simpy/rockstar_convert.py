4dd3945e7a3db7547f9a6d1245ed7d9b8ac63703
mtremmel <m.tremmel6@gmail.com>
2015-10-28 11:05:18 -0700

    fix name reference in rockstar_iord_to_fpos

--- Simpy/rockstar_convert.py
+++ Simpy/rockstar_convert.py
@@ -10,7 +10,7 @@ def rockstar_iord_to_fpos(snapn):
 	iord_to_fpos = util.init_iord_to_fpos(snap)
 	del(snap)
 	gc.collect()
-	f = open(snap+'.rockstar.halo_particles','rb')
+	f = open(snapn+'.rockstar.halo_particles','rb')
 	orig = np.fromfile(f,dtype=np.dtype('int64'),count=-1)
 	f.close()
 	return iord_to_fpos[orig]
