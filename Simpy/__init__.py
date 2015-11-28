4f3f776a5ff0f9ace76191f42a1fcbc92d60a3d9
mtremmel <m.tremmel6@gmail.com>
2015-11-28 10:24:48 -0800

    change halo_db to HaloDB to avoid confusion

--- Simpy/__init__.py
+++ Simpy/__init__.py
@@ -1,4 +1,4 @@
 from . import Files, cosmology, util, rockstar_convert
-from . import BlackHoles, Stars, Relations
+from . import BlackHoles, Stars, Relations, halo_db
 from .readcol import readcol
 
