27d7eed9f5a54f1864345f61387093f60e004eeb
mtremmel <m.tremmel6@gmail.com>
2015-11-29 15:08:02 -0800

    change name of function to track_halo_bh_acc

--- Simpy/HaloDB/bhacc_hist.py
+++ Simpy/HaloDB/bhacc_hist.py
@@ -2,7 +2,7 @@ import numpy as np
 import pynbody
 
 
-def gen_bh_acc(simname, endstep, halonum, bhorbit, active=1e42):
+def track_halo_bh_acc(simname, endstep, halonum, bhorbit, active=1e42):
 	import halo_db as db
 	hcur = db.get_halo(simname+'/%'+str(endstep)+'/'+str(halonum))
 	h = hcur
@@ -103,6 +103,8 @@ def gen_bh_acc(simname, endstep, halonum, bhorbit, active=1e42):
 			   }
 
 	return AccHist
+
+
 
 
 
