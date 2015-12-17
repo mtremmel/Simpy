bb915abd4fbaec3ba5b2fa078ff8a022d9c0a287
mtremmel <m.tremmel6@gmail.com>
2015-12-17 14:40:18 -0800

    make 30 size font the default for plots

--- Simpy/plotting.py
+++ Simpy/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 plt.ion()
 plt.rc('xtick',labelsize=15)
 plt.rc('ytick',labelsize=15)
-plt.rc('font', weight='medium')
+plt.rc('font', weight='medium', size=30)
 plt.rc('axes', linewidth=2)
 plt.rc('xtick.major',width=2)
 plt.rc('ytick.major',width=2)
