4b61f8cad40ca13bd33f46e144ee7da6bbd60157
mtremmel <m.tremmel6@gmail.com>
2015-12-17 15:45:58 -0800

    fix colorbar tick labels

--- Simpy/Stars/color_hist.py
+++ Simpy/Stars/color_hist.py
@@ -77,9 +77,9 @@ class ColorHist(object):
 
 
         if cbar is True and overplot is False:
-            cbar = plotting.plt.colorbar(ticks=[0.25,0.33,0.5,1])
+            cbar = plotting.plt.colorbar(ticks=[0.25, 1./3., 0.5, 1./1.5, 1])
             cbar.set_label('Redshift', fontsize=30)
-            cbar.set_ticklabels(['3','2','1','0'])
+            cbar.set_ticklabels(['3','2','1','0.5','0'])
 
         if overplot is False:
             plotting.plt.xlabel(c1+'-'+c2+' color')
