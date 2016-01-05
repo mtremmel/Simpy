b4ed626ed804b72413f0e60428ff70a7956a0590
mtremmel <m.tremmel6@gmail.com>
2016-01-05 09:29:37 -0800

    change default font size in plotting.py

--- Simpy/plotting.py
+++ Simpy/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 plt.ion()
 plt.rc('xtick',labelsize=25)
 plt.rc('ytick',labelsize=25)
-plt.rc('font', weight='medium', size=35)
+plt.rc('font', weight='medium', size=40)
 plt.rc('axes', linewidth=2)
 plt.rc('xtick.major',width=2)
 plt.rc('ytick.major',width=2)
