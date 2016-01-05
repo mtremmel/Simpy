3d33c2554b259b6225baf24a4bd0feac84bb8e11
mtremmel <m.tremmel6@gmail.com>
2016-01-05 09:26:31 -0800

    change defaults to plotting environment

--- Simpy/plotting.py
+++ Simpy/plotting.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
 plt.ion()
-plt.rc('xtick',labelsize=15)
-plt.rc('ytick',labelsize=15)
-plt.rc('font', weight='medium', size=30)
+plt.rc('xtick',labelsize=25)
+plt.rc('ytick',labelsize=25)
+plt.rc('font', weight='medium', size=35)
 plt.rc('axes', linewidth=2)
 plt.rc('xtick.major',width=2)
 plt.rc('ytick.major',width=2)
