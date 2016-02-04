c267a60862621f221b1cce6b06efd404f95d3f7e
mtremmel <m.tremmel6@gmail.com>
2016-02-04 08:53:15 -0800

    use steps.list rather than files.list in rockstar convert.... less chance of mistakes when dealing with symbolic links

--- Simpy/rockstar_convert.py
+++ Simpy/rockstar_convert.py
@@ -17,22 +17,23 @@ def rockstar_iord_to_fpos(snapn):
 
 def convert_all_rockstar(simname):
 	Files.cklists(simname)
-	f = open('files.list','r')
-	files = f.readlines()
+	f = open('steps.list','r')
+	steps = f.readlines()
 	f.close()
-	for ff in files:
+	for ff in steps:
 		snapn = ff.strip('\n')
-		print snapn
-		if not os.path.exists(snapn+'.rockstar.halo_particles'):
+		snap = simname+snapn
+		print snap
+		if not os.path.exists(snap+'.rockstar.halo_particles'):
 			print "snapshot does not have halo_particles file... ignoring this snapshot..."
 			continue
-		if os.path.exists(snapn+'.rockstar.halo_particles_fpos'):
+		if os.path.exists(snap+'.rockstar.halo_particles_fpos'):
 			print "fpos file already found! skipping..."
 			continue
-		fpos = rockstar_iord_to_fpos(snapn)
+		fpos = rockstar_iord_to_fpos(snap)
 		if type(fpos[-1]) != np.int64:
 			fpos = fpos.astype(np.int64)
-		outf = open(snapn+'.rockstar.halo_particles_fpos','wb')
+		outf = open(snap+'.rockstar.halo_particles_fpos','wb')
 		fpos.tofile(outf, format='%l')
 		del(fpos)
 		gc.collect()
