08814c0983e5fdc8d3dc62b8a8f8b70125f2fcf2
mtremmel <m.tremmel6@gmail.com>
2016-03-21 16:45:04 -0700

    allow for merging BHs to not be in snapshot (i.e. the form and merge between snapshots)

--- Simpy/BlackHoles/hosts.py
+++ Simpy/BlackHoles/hosts.py
@@ -216,21 +216,23 @@ class StepData(object):
                         'lum1': np.zeros(len(ID1)), 'lum2': np.zeros(len(ID1)), 'kick':kick}
 
         for i in range(len(ID1)):
-            o1 = np.where(self.bh['bhid']==ID1[i])[0][0]
-            o2 = np.where(self.bh['bhid']==ID2[i])[0][0]
-
-            h1 = self.bh['host'][o1]
-            self.mergers['halo'][i] = h1
-            h2 = self.bh['host'][o2]
-            self.mergers['eaten_halo'][i] = h2
-            m1 = self.bh['mass'][o1]
-            m2 = self.bh['mass'][o2]
-            self.mergers['mass1'][i] = m1
-            self.mergers['mass2'][i] = m2
-            md1 = self.bh['mdot'][o1]
-            md2 = self.bh['mdot'][o2]
-            self.mergers['lum1'][i] = calc_lum(md1)
-            self.mergers['lum2'][i] = calc_lum(md2)
+            o1, = np.where(self.bh['bhid']==ID1[i])
+            o2, = np.where(self.bh['bhid']==ID2[i])
+            if len(o1) > 0:
+                o1 = o1[0]
+                h1 = self.bh['host'][o1]
+                self.mergers['halo'][i] = h1
+                m1 = self.bh['mass'][o1]
+                self.mergers['mass1'][i] = m1
+                md1 = self.bh['mdot'][o1]
+                self.mergers['lum1'][i] = calc_lum(md1)
+            if len(o2) > 0:
+                h2 = self.bh['host'][o2]
+                self.mergers['eaten_halo'][i] = h2
+                m2 = self.bh['mass'][o2]
+                self.mergers['mass2'][i] = m2
+                md2 = self.bh['mdot'][o2]
+                self.mergers['lum2'][i] = calc_lum(md2)
 
 
             h1index = np.where(self.host_ids==h1)[0][0]
