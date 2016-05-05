slightly change grav. constant and fix ommision of pi in strain calculation

Simpy/BlackHoles/mergers.py
@@ -152,7 +152,7 @@ def strain(M1, M2, z, omegaM, omegaL, h0, eps=0.1, a=0.95):
     dL = cosmology.lum_distance(z,omegaM, omegaL, h0)
     dL *= 3.086e+24
     Ef = dEdf(M1, M2, eps, a)
-    return np.sqrt(2.*util.G/util.c**3) * ((1+z)/dL) * np.sqrt(Ef)
+    return np.sqrt(2.*util.G/util.c**3) * ((1+z)/(np.pi*dL)) * np.sqrt(Ef)
 
 
 

Simpy/util.py
@@ -4,7 +4,7 @@ import scipy
 #useful constants
 c = 2.99792458e10
 lbol_sun = 3.9e33
-G = 6.67384e-8
+G = 6.67259e-8
 M_sun_g = 1.988547e33
 loglbol_sun = np.log10(lbol_sun)
 