aa0db30ab5485a144704e2a3f89734aed8205464
mtremmel <m.tremmel6@gmail.com>
2016-07-03 10:49:56 -0700

    change properties gathered to ensure link data is a subset of halodat

--- Simpy/Files.py
+++ Simpy/Files.py
@@ -128,10 +128,10 @@ def create_halo_tree_files(dbsim, h=0.6776931508813172, nmax=None):
 	import pynbody
 	for step in dbsim.timesteps:
 		halodat = step.gather_property('halo_number()', 'Mvir', 'vmax_dm_global()', 'Rvir', 'NDM()', 'NStar()', 'NGas()', 'SSC', 'Vcom')
-		halo_linkdat = step.gather_property('halo_number()','vmax_dm_global()','later(1).halo_number()','later(1).vmax_dm_global()')
+		halo_linkdat = step.gather_property('halo_number()','vmax_dm_global()','Vcom','later(1).halo_number()','later(1).vmax_dm_global()','later(1).Vcom')
 
 		desc = np.ones(len(halodat[0]))*-1
-		desc[(np.in1d(halodat[0],halo_linkdat[0]))] = halo_linkdat[2]
+		desc[(np.in1d(halodat[0],halo_linkdat[0]))] = halo_linkdat[3]
 		ID = halodat[0]
 		Mvir = halodat[1] * h
 		Vmax = halodat[2]
