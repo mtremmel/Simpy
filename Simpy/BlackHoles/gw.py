9c879f97ddd564243aae6341310a87585acdc723
mtremmel <m.tremmel6@gmail.com>
2016-07-28 14:20:31 -0700

    rework organization of GW calculations. put in a tracer for BH mergers that never are in different halos. calculate rawdata redshifts automatically

---
+++ Simpy/BlackHoles/gw.py
@@ -0,0 +1,140 @@
+from .. import util, cosmology
+import numpy as np
+
+class GWemit(object):
+#GW emission approx. without spin given by Ajith+ 2008
+# (http://journals.aps.org/prd/pdf/10.1103/PhysRevD.77.104017)
+	def __init__(self, mass1, mass2, z,omegaM, omegaL, h):
+		self.masses = (mass1, mass2)
+		self.z = z
+		self.cosmology = {'omegaM': omegaM, 'omegaL': omegaL, 'h':h}
+
+
+
+	def freq(self, a, b, c):
+		M = self.masses[0]+self.masses[1]
+		n = self.masses[0]*self.masses[1]/M**2
+
+		return (a*n**2 + b*n + c)/(np.pi*M) * util.c.in_units('cm s**-1')**3/util.G.in_units('Msol**-1 cm**3 s**-1')
+
+	def freq_merge(self):
+		a = 2.9740e-1
+		b = 4.4810e-2
+		c = 9.5560e-2
+		return self.freq(a,b,c)
+
+	def freq_ring(self):
+		a = 5.94110e-1
+		b = 8.97940e-2
+		c = 1.91110e-1
+		return self.freq(a,b,c)
+
+	def freq_cut(self):
+		a = 8.49450e-1
+		b = 1.28480e-1
+		c = 2.72990e-1
+		return self.freq(a,b,c)
+
+	def sigfreq(self):
+		a = 5.08010e-1
+		b = 7.75150e-2
+		c = 2.23690e-1
+		return self.freq(a,b,c)
+
+	def ampGW(self, f):
+		M = self.masses[0]+self.masses[1]
+		n = self.masses[0]*self.masses[1]/M**2
+		dL = cosmology.lum_distance(self.z,self.cosmolggy['omegaM'], self.cosmolgoy['omegaL'],self.cosmology['h'])
+
+		dL *= 3.086e+24
+		C = (util.G.in_units('Mpc**3 Msol**-1 s**-2')/util.c.in_untits('Mpc s**-1')) * \
+			M**(5./6.) * self.freq_merge()**(-7./6.) * (5.*n/24.)**0.5 / (dL*np.pi**(2./3.))
+
+		fm = self.freq_merge()
+		fr = self.freq_ring()
+		fc = self.freq_cut()
+		sig = self.sigfreq()
+
+		def LL(sig,f,fr):
+			return (1/2.*np.pi)*sig/((f-fr)**2 + sig**2/4.)
+
+		if f < fm:
+			return C * (f/fm)**(-7./6.)
+		if f >= fm and f < fr:
+			return C * (f/fm)**(-2./3.)
+		if f >= fr and f < fc:
+			return C * np.pi*sig/2. * (fr/fm)**(-2./3.) * LL(sig,f,fr)
+		if f >=fc:
+			print "ERROR frequency > f_cut"
+			raise RuntimeError
+
+	def ampGW_merger(self):
+		return self.ampGW(self.freq_merge())
+
+	def ampGW_ring(self):
+		return self.ampGW(self.freq_ring())
+
+
+class SalcidoGW(object):
+	# GW calculations based on Salcido+ 2015, Flanagen + Hughes 1998, and others
+	def __init__(self, mass1, mass2, z, omegaM, omegaL, h):
+		self.masses = (mass1, mass2)
+		self.z = z
+		self.cosmology = {'omegaM': omegaM, 'omegaL': omegaL, 'h':h}
+
+	def freq_qnr(self, a=0.95):
+		Mtot = self.masses[0] + self.masses[1]
+		Mtot *= util.M_sun_g
+		return util.c**3 * (1 - 0.63*(1-a)**(3./10.)) / (2 * np.pi * util.G * Mtot)
+
+	def freq_merger(self):
+		Mtot = self.masses[0] + self.masses[1]
+		return util.c**3 * 0.02 / (util.G * Mtot)
+
+	def dEdf(self, eps=0.1, a=0.95):
+		fqnr = self.freq_qnr(a)
+		fm = self.freq_merger()
+		M1 = self.masses[0]
+		M2 = self.masses[1]
+		M1 *= util.M_sun_g
+		M2 *= util.M_sun_g
+		F = (4.*M1*M2)**2/(M1+M2)**4
+		return util.c**2*(M1+M2)*F*eps/(fqnr-fm)
+
+	def strain(self, eps=0.1, a=0.95):
+		dL = cosmology.lum_distance(self.z,self.cosmology['omegaM'], self.cosmology['omegaL'], self.cosmology['h'])
+		dL *= 3.086e+24
+		Ef = self.dEdf(eps, a)
+		return np.sqrt(2.*util.G/util.c**3) * ((1+self.z)/(np.pi*dL)) * np.sqrt(Ef)
+
+def eLisaLimitLPF(fobs, config=1):
+	def sacc(fobs):
+		return 1.551245e-29*(1 + (4.48833e-4/fobs)**2 + (fobs/0.0636734)**4) / (2*np.pi*fobs)**4
+
+	somn = 8.65e-24
+
+	if config == 1:
+		ssn = 4.39e-23
+	if config == 2:
+		ssn =  3.90e-23
+	if config == 5:
+		ssn = 5.86e-23
+
+	L = config * 1.0e6
+
+	def sn(fobs):
+		return 20./3. * (4.*sacc(fobs)+ssn+somn)/L**2 * (1+(fobs/(0.41*(util.c/(1.0e2*2.0*L))))**2)
+
+	return np.sqrt(sn(fobs)*fobs)
+
+#eLisa limits from Salcido+2015, taken from Amaro-Seoane+ 2013
+def eLisaLimit_old(fobs):
+	def sacc(fobs):
+		return 1.37e-32 * (1+1e-4/fobs)*(fobs)**-4
+	ssn = 5.25e-23
+	somn = 6.28e-23
+	L = 1.0e9
+	def sn(fobs):
+		return 20./3. * (4.*sacc(fobs)+ssn+somn)/L**2 * (1+(fobs/(0.41*(util.c/(1.0e2*2.0*L))))**2)
+
+	return np.sqrt(sn(fobs)*fobs)
--- Simpy/BlackHoles/mergers.py
+++ Simpy/BlackHoles/mergers.py
@@ -134,131 +134,9 @@ def plt_merger_rates(time,sim, color='b',linestyle='-', vol_weights=1./25.**3, b
     if ret_data is True:
         return rate, tzbins,tedges
 
-# GW calculations based on Salcido+ 2015, Flanagen + Hughes 1998, and others
-def freq_qnr(Mtot, a=0.95):
-    Mtot *= util.M_sun_g
-    return util.c**3 * (1 - 0.63*(1-a)**(3./10.)) / (2 * np.pi * util.G * Mtot)
-
-def freq_merger(Mtot):
-    Mtot *= util.M_sun_g
-    return util.c**3 * 0.02 / (util.G * Mtot)
-
-def dEdf(M1, M2, eps=0.1, a=0.95):
-    fqnr = freq_qnr(M1+M2,a)
-    fm = freq_merger(M1+M2)
-    M1 *= util.M_sun_g
-    M2 *= util.M_sun_g
-    F = (4.*M1*M2)**2/(M1+M2)**4
-    return util.c**2*(M1+M2)*F*eps/(fqnr-fm)
-
-def strain(M1, M2, z, omegaM, omegaL, h0, eps=0.1, a=0.95):
-    dL = cosmology.lum_distance(z,omegaM, omegaL, h0)
-    dL *= 3.086e+24
-    Ef = dEdf(M1, M2, eps, a)
-    return np.sqrt(2.*util.G/util.c**3) * ((1+z)/(np.pi*dL)) * np.sqrt(Ef)
-
-#eLisa limits from Salcido+2015, taken from Amaro-Seoane+ 2013
-def eLisaLimit(fobs):
-    def sacc(fobs):
-        return 1.37e-32 * (1+1e-4/fobs)*(fobs)**-4
-    ssn = 5.25e-23
-    somn = 6.28e-23
-    L = 1.0e9
-    def sn(fobs):
-        return 20./3. * (4.*sacc(fobs)+ssn+somn)/L**2 * (1+(fobs/(0.41*(util.c/(1.0e2*2.0*L))))**2)
-
-    return np.sqrt(sn(fobs)*fobs)
-
-#GW emission approx. without spin given by Ajith+ 2008
-# (http://journals.aps.org/prd/pdf/10.1103/PhysRevD.77.104017)
-def freq(a, b, c, M1, M2):
-    M = M1 + M2
-    n = M1*M1/M**2
-
-    return (a*n**2 + b*n + c)/(np.pi*M) * util.c.in_units('cm s**-1')**3/util.G.in_units('Msol**-1 cm**3 s**-1')
-
-def freq_merge(M1, M2):
-    a = 2.9740e-1
-    b = 4.4810e-2
-    c = 9.5560e-2
-    return freq(a,b,c,M1, M2)
-
-def freq_ring(M1, M2):
-    a = 5.94110e-1
-    b = 8.97940e-2
-    c = 1.91110e-1
-    return freq(a,b,c,M1, M2)
-
-def freq_cut(M1, M2):
-    a = 8.49450e-1
-    b = 1.28480e-1
-    c = 2.72990e-1
-    return freq(a,b,c,M1, M2)
-
-def sigfreq(M1, M2):
-    a = 5.08010e-1
-    b = 7.75150e-2
-    c = 2.23690e-1
-    return freq(a,b,c,M1, M2)
-
-def ampGW(M1, M2, z, f, omegaM, omegaL, h0):
-    M = M1 + M2
-    n = M1*M2/M**2
-    dL = cosmology.lum_distance(z,omegaM, omegaL, h0)
-    dL *= 3.086e+24
-
-    C = (util.G.in_units('Mpc**3 Msol**-1 s**-2')/util.c.in_untits('Mpc s**-1')) * \
-        M**(5./6.) * freq_merge(M1,M2)**(-7./6.) * (5.*n/24.)**0.5 / (dL*np.pi**(2./3.))
-
-    fm = freq_merge(M1,M2)
-    fr = freq_ring(M1,M2)
-    fc = freq_cut(M1,M2)
-    sig = sigfreq(M1,M2)
-
-    def LL(sig,freq,fr):
-        return (1/2.*np.pi)*sig/((f-fr)**2 + sig**2/4.)
-
-    if f < fm:
-        return C * (f/fm)**(-7./6.)
-    if f >= fm and f < fr:
-        return C * (f/fm)**(-2./3.)
-    if f >= fr and f < fc:
-        return C * np.pi*sig/2. * (fr/fm)**(-2./3.) * LL(sig,f,fr)
-    if f >=fc:
-        print "ERROR frequency > f_cut"
-        raise RuntimeError
-
-
-
-#from private communications with Marta, taken based off of latest Pathfinder data
-def eLisaLimitLPF(fobs, config=1, dogal=False):
-    def sacc(fobs):
-        return 1.551245e-29*(1 + (4.48833e-4/fobs)**2 + (fobs/0.0636734)**4) / (2*np.pi*fobs)**4
-
-    somn = 8.65e-24
-
-    if config == 1:
-        ssn = 4.39e-23
-    if config == 2:
-        ssn =  3.90e-23
-    if config == 5:
-        ssn = 5.86e-23
-
-    L = config * 1.0e6
-
-    def sn(fobs):
-        return 20./3. * (4.*sacc(fobs)+ssn+somn)/L**2 * (1+(fobs/(0.41*(util.c/(1.0e2*2.0*L))))**2)
-
-    return np.sqrt(sn(fobs)*fobs)
-
-
-
-
-
-
 
 class mergerCat(object):
-    def __init__(self, dbsim, simname, properties=[], bhorbit=None):
+    def __init__(self, dbsim, simname, properties=['Mvir', 'Mstar', 'Mgas']):
         proplist = ['halo_number()', 'BH_merger_next.halo_number()', 'host_halo.halo_number()',
                     'BH_merger_next.host_halo.halo_number()', 'BH_merger_next.earlier(1).host_halo.halo_number()']
         for prop in properties:
@@ -284,18 +162,21 @@ class mergerCat(object):
         ordr = np.argsort(self.rawdat['ID2'])
         util.cutdict(self.rawdat,ordr)
 
+        s = dbsim.timestep[0].load()
+        scale, red = cosmology.getScaleFactor(self.rawdat['time'],s)
+
         uIDeat, cnt = np.unique(self.rawdat['ID2'], return_counts=True)
         if len(np.where(cnt>1)[0])>0:
             print "SHIIIIIIT"
 
         self.data = {'ID1':[], 'ID2':[], 'ratio':[], 'kick':[], 'step':[],
                     'time':[], 'tsnap_prev':[], 'tsnap_after':[], 'snap_prev':[], 'snap_after':[],
-                    'host_N_pre_1':[], 'host_N_pre_2':[], 'host_N_post':[]}
+                    'host_N_1':[], 'host_N_2':[], 'host_N_f':[]}
 
         for p in properties:
-            self.data[p+'_pre_1'] = []
-            self.data[p+'_pre_2'] = []
-            self.data[p+'_post'] = []
+            self.data[p+'_1'] = []
+            self.data[p+'_2'] = []
+            self.data[p+'_f'] = []
 
 
         self.nmergers = []
@@ -325,9 +206,9 @@ class mergerCat(object):
                 print "Double counted IDs: ", tt1[(tt2>1)]
                 raise RuntimeError("ERROR double counted IDeat in database analysis")
 
-            self.data['host_N_pre_1'].extend(data[4])
-            self.data['host_N_pre_2'].extend(data[2])
-            self.data['host_N_post'].extend(data[3])
+            self.data['host_N_1'].extend(data[4])
+            self.data['host_N_2'].extend(data[2])
+            self.data['host_N_f'].extend(data[3])
 
             self.data['snap_after'].extend(int(stepnumA) * np.ones(len(data[1])).astype(np.int))
             self.data['snap_prev'].extend(int(stepnum) * np.ones(len(data[1])).astype(np.int))
@@ -336,9 +217,9 @@ class mergerCat(object):
 
             index = 5
             for i in range(len(properties)):
-                self.data[properties[i]+'_pre_1'].extend(data[index+2])
-                self.data[properties[i]+'_pre_2'].extend(data[index])
-                self.data[properties[i]+'_post'].extend(data[index+1])
+                self.data[properties[i]+'_1'].extend(data[index+2])
+                self.data[properties[i]+'_2'].extend(data[index])
+                self.data[properties[i]+'_f'].extend(data[index+1])
                 index += 3
 
         for key in self.data.keys():
@@ -371,27 +252,28 @@ class mergerCat(object):
             self.data[p][ordee[match]] = self.rawdat[p][match2]
 
 
-    def calc_GW_emission(self, sim, eps=0.1, a=0.95):
-        self.rawdat['GW_freq_merge'] = np.ones(len(self.rawdat['ID1']))*-1
-        self.rawdat['GW_strain_merge'] = np.ones(len(self.rawdat['ID1']))*-1
+    def calc_GW_emission(self, sim):
+        from . import gw
 
         if 'merge_mass_1' not in self.rawdat.keys():
             print "ERROR need to find final masses for BHs"
             return
 
         ok = np.where((self.rawdat['merge_mass_1'] > 0)&(self.rawdat['merge_mass_2']>0))[0]
-        if "redshift" in self.rawdat.keys():
-            z = self.rawdat['redshift']
-        else:
-            z = pynbody.analysis.cosmology.redshift(sim, self.rawdat['time'])
-            self.rawdat['redshift'] = z
-        hc = strain(self.rawdat['merge_mass_1'][ok], self.rawdat['merge_mass_2'][ok], self.rawdat['redshift'][ok],
-                    sim.properties['omegaM0'], sim.properties['omegaL0'], sim.properties['h'], eps=eps, a=a)
-        fm = freq_merger(self.rawdat['merge_mass_1']+self.rawdat['merge_mass_2'])
-        self.rawdat['GW_freq_merge'][ok] = fm
-        self.rawdat['GW_strain_merge'][ok] = hc
 
-        self._match_data_to_raw('GW_freq_merge', 'GW_strain_merge')
+        self.gwemit = gw.GWemit(self.rawdat['merge_mass_1'][ok], self.rawdat['merge_mass_2'][ok],self.rawdat['redshift'])
+
+        self.rawdat['GW_freq_merge'] = np.ones(len(self.rawdat['ID1']))*-1
+        self.rawdat['GW_strain_merge'] = np.ones(len(self.rawdat['ID1']))*-1
+        self.rawdat['GW_freq_ring'] = np.ones(len(self.rawdat['ID1']))*-1
+        self.rawdat['GW_strain_ring'] = np.ones(len(self.rawdat['ID1']))*-1
+
+        self.rawdat['GW_freq_merge'][ok] = self.gwemit.freq_merge()
+        self.rawdat['GW_freq_ring'][ok] = self.gwemit.freq_ring()
+        self.rawdat['GW_strain_merge'][ok] = self.gwemit.ampGW_merger()
+        self.rawdat['GW_strain_ring'][ok] = self.gwemit.ampGW_ring()
+
+        self._match_data_to_raw('GW_freq_merge', 'GW_freq_ring', 'GW_strain_merge', 'GW_strain_ring')
 
 
 
@@ -531,6 +413,7 @@ class mergerCat(object):
         if 'dt_hmerger' not in self.data.keys() or overwrite==True:
             self.data['dt_hmerger'] = np.ones(len(self.data['ID1']))*-1
             self.data['dt_hmerger_min'] = np.ones(len(self.data['ID1']))*-1
+        nodiff = 0
         for i in range(len(self.data['ID1'])):
             if i%30 == 0:
                 print float(i)/float(len(self.data['ID1']))*100, '% done'
@@ -552,6 +435,7 @@ class mergerCat(object):
                 print "WARNING time arrays don't match!"
             diff = np.where(hn1[match1]!=hn2[match2])[0]
             if len(diff)==0 or len(match1)==0 or len(match2)==0:
+                nodiff += 1
                 time1_all =  bh1.reverse_property_cascade('t()')
                 time2_all =  bh2.reverse_property_cascade('t()')
                 if len(time1_all)==0 or len(time2_all)==0:
@@ -584,6 +468,7 @@ class mergerCat(object):
                 self.data['dt_hmerger_min'][i] = self.data['time'][i] - th1p
             else:
                 self.data['dt_hmerger_min'][i] = 0
+        print "finished with ", nodiff, "BHs having never been in different halos"
 
 
 
