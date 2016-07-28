cc14ce1fd7d9d4d561a0ffda102533a361e2476e
mtremmel <m.tremmel6@gmail.com>
2016-07-28 15:08:51 -0700

    fix typo

--- Simpy/Relations/halos.py
+++ Simpy/Relations/halos.py
@@ -294,7 +294,7 @@ def mergerhist(dbsim, moreprops=None, names=None, ret_totals=False, nsteps = Non
 
     for p in moreprops:
         allprops.append(p)
-        allprops.append('later(1)'+p)
+        allprops.append('later(1).'+p)
 
     print allprops
 
