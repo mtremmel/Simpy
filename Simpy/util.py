fix unit conversion

Simpy/util.py
@@ -72,9 +72,8 @@ def get_rates_by_z(zin,s,range=[0,20],nbins=40,units='yr**-1',weights=None):
     from . import cosmology
     n,zbins = np.histogram(zin,weights=weights,range=range,bins=nbins)
     tedges = pynbody.array.SimArray([cosmology.getTime(z,s) for z in zbins],'Gyr')
-    tedges = tedges.in_units(units)
     dt = np.abs(tedges[0:-1]-tedges[1:])
     n = n/dt
-    return n, zbins
+    return n.in_units(units), zbins
 
 