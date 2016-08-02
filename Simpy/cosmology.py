88aa6511756d97d28aeb7e6d66e1e86edb20fa7d
mtremmel <m.tremmel6@gmail.com>
2016-08-02 10:49:33 -0700

    fix error in calc_rho

--- Simpy/cosmology.py
+++ Simpy/cosmology.py
@@ -85,7 +85,7 @@ class HMF(object):
                         print "value for logM excedes maximum value calculated"
                         raise ValueError
                 i = int((logm - self.minlm)/self.delta)
-                return self.hmf[step][i][i]*self.delta
+                return self.hmf[step][1][i]*self.delta
 
 
 
