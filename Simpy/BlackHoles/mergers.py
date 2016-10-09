fix issue with time calculation

Simpy/BlackHoles/mergers.py
@@ -715,9 +715,9 @@ class mergerCat(object):
             close = np.where(dist<maxD)[0]
             if len(close) > 0:
                 try:
-                    dt = np.sum(time1[use1[close]] - time1[use1[close]-1])
+                    dt = np.sum(np.abs(time1[use1[close]] - time1[use1[close]-1]))
                 except:
-                    dt = np.sum(time1[use1[close]] - time1[use1[close]+1])
+                    dt = np.sum(np.abs(time1[use1[close]] - time1[use1[close]+1]))
                 self.rawdat[tstr][i] = dt
                 dual = np.where((lum1[use1[close]]>minL)&(lum2[use2[close]]>minL))[0]
                 self.rawdat[fstr][i] = float(len(dual))/float(len(close))