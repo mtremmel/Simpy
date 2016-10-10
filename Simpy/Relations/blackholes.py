671977abde68d144827022ad2b53e3f6e005733a
mtremmel <m.tremmel6@gmail.com>
2016-10-10 08:39:21 -0700

    fix issue with print statement

--- Simpy/Relations/blackholes.py
+++ Simpy/Relations/blackholes.py
@@ -83,7 +83,10 @@ def Find_AGN(dbsim, lAGN=1e43):
 
         uid, ind, cnt = np.unique(host,return_index=True,return_counts=True)
         for i in range(len(uid)):
-            if i%int(len(uid)/10.)==0: print float(i)/len(uid)*100,'% done'
+            if len(uid)>10
+                if i%int(len(uid)/10.)==0: print float(i)/len(uid)*100,'% done'
+            else:
+                print float(i)/len(uid)*100, '% done'
             lum = mdot[ind[i]:ind[i]+cnt]*util.c**2*util.M_sun_g/(3600.*24.*365.)
             dist = distance[ind[i]:ind[i]+cnt]
             mass = mass[ind[i]:ind[i]+cnt]
