8b67d2ab0ee919ab968cae8477d43b000668c8ac
mtremmel <m.tremmel6@gmail.com>
2017-02-01 16:18:02 -0800

    make output optional

--- Simpy/cosmology.py
+++ Simpy/cosmology.py
@@ -10,11 +10,11 @@ def getTime(z,sim):
         c = cosmo.Cosmology(sim=sim)
         return other_cosmo.age(sim,z=0)*c.Exp2Time(1.0 / (1+z))/c.Exp2Time(1)
 
-def getScaleFactor(times,s):
+def getScaleFactor(times,s, verbose=False):
         redshift = np.zeros(np.size(times))
         ntimes = np.size(times)
         for tt in range(np.size(times)):
-                if tt%100==0:
+                if tt%100==0 and verbose is True:
                         print tt/np.float(np.size(times)) * 100, '% done'
                 def func(z):
                         return getTime(z,s) - times.in_units('Gyr')[tt]
