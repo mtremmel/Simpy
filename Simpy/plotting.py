df0adb191ef0c45720d26dc61db9c14ab3615fba
mtremmel <m.tremmel6@gmail.com>
2017-02-14 07:56:11 -0800

    add colorbar option

--- Simpy/plotting.py
+++ Simpy/plotting.py
@@ -9,7 +9,7 @@ plt.rc('xtick.major',width=2, pad=10)
 plt.rc('ytick.major',width=2, pad=5)
 
 
-def two_d_grid(data,tick_array1,tick_array2,color_norm,cmap,noticks=True):
+def two_d_grid(data,tick_array1,tick_array2,color_norm,cmap,noticks=True,cbar=True,cbarlabel=None):
 	plt.imshow(data,norm=color_norm,cmap=cmap)
 	if noticks is True:
 		plt.xticks([])
@@ -17,6 +17,10 @@ def two_d_grid(data,tick_array1,tick_array2,color_norm,cmap,noticks=True):
 	else:
 		plt.xticks(np.arange(len(data[0])),tick_array1)
 		plt.yticks(np.arange(len(data[:,0])),tick_array2)
+	if cbar is True:
+		cbar = plt.colorbar()
+		if cbarlabel:
+			cbar.set_label('Redshift', fontsize=30, labelpad=2)
 	return
 
 def name_ticks_2d(xnames,ynames):
@@ -24,7 +28,7 @@ def name_ticks_2d(xnames,ynames):
 	plt.yticks(np.arange(len(xnames)),ynames[::-1])
 
 def make_two_d_plot(data1, bins1, data2, bins2, zdata, cmap, zdata_function = np.mean,
-					zdata_range=None, makeplot=True, return_data=False):
+					zdata_range=None, makeplot=True, return_data=False, cbar=True, cbarlabel=None):
 
 	image = np.zeros((len(bins1)-1,len(bins2)-1))
 
@@ -40,7 +44,7 @@ def make_two_d_plot(data1, bins1, data2, bins2, zdata, cmap, zdata_function = np
 		else:
 			norm = colors.Normalize(image.min(),image.max())
 
-		two_d_grid(image,bins1[:-1],bins2[:-1],norm,cmap,noticks=True)
+		two_d_grid(image,bins1[:-1],bins2[:-1],norm,cmap,noticks=True,cbar=cbar,cbarlabel=cbarlabel)
 
 	if return_data is True:
 		return image
