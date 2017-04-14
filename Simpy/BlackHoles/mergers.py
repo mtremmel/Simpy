51dc05cd5cbbb2ba5d32675ebddec50d973f3d86
mtremmel <m.tremmel6@gmail.com>
2017-04-14 12:59:37 -0700

    try another fix

--- Simpy/BlackHoles/mergers.py
+++ Simpy/BlackHoles/mergers.py
@@ -1104,7 +1104,7 @@ class mergerCat(object):
                 dt = len(close)*timestep
             else:
                 if np.min(use1[close])==0:
-                    dt = np.sum(time1[use1[close[1:]]] - time1[use1[close[1:]]-1])
+                    dt = np.sum(time1[use1[close[(use1[close]>0)]]] - time1[use1[close[(use1[close]>0)]]-1])
                 else:
                     dt = np.sum(np.abs(time1[use1[close]] - time1[use1[close]-1]))
 
