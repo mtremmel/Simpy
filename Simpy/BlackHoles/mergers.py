fix units issue

Simpy/BlackHoles/mergers.py
@@ -1141,13 +1141,13 @@ class mergerCat(object):
         from .. import util
         z = np.copy(self.rawdat['redshift'])
         z[(z>2)]=2
-        sigma = 190 * (self.rawdat['Mstar']/1e11)**0.2 * (1+z)**0.44
+        sigma = pynbody.array.SimArray(190 * (self.rawdat['Mstar']/1e11)**0.2 * (1+z)**0.44,'km s**-1')
         if usenewmass:
             mbh = np.minimum(self.rawdat['newmass2'], self.rawdat['newmass1'])
         else:
             mbh = np.minimum(self.rawdat['merge_mass_2'],self.rawdat['merge_mass_1'])
         #rsch = Simpy.util.G.in_units('km**3 s**-2 Msol**-1') * mbh/Simpy.util.c.in_units('km s**-1')**2
-        r90 = util.G.in_units('km**3 s**-2 Msol**-1')*mbh/sigma**2
+        r90 = util.G.in_units('kpc**3 s**-2 Msol**-1')*mbh/sigma.in_units('kpc s**-1')**2
         lnlam = np.log(r/r90)
 
         tdf = 19/lnlam * (r/5)**2 * (sigma/200.) * (1e8/mbh)